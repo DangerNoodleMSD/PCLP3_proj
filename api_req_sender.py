f97551cce32434c8e7b055f43995d6bc860aa03d
Dobrea Matei-Stefan <matei.dobrea22@gmail.com>
2026-05-27 14:47:15 +0300

    csv and json response

--- api_req_sender.py
+++ api_req_sender.py
@@ -1,10 +1,15 @@
 import requests
 
-f = open("response.txt", "w")
+f = open("response_metadata.json", "w")
 
-response = requests.get('https://api.tidesandcurrents.noaa.gov/api/prod/datagetter?date=today&station=9419750&product=water_level&datum=MTL&time_zone=gmt&units=metric&format=csv')
-
+response = requests.get('https://api.tidesandcurrents.noaa.gov/mdapi/prod/webapi/stations.json?type=waterlevels&units=english')
 if (response.status_code == 200):
 	f.write(response.text)
+f.close()
 
-print(response.text)
+f = open("response_data.csv", "w")
+
+response = requests.get('https://api.tidesandcurrents.noaa.gov/api/prod/datagetter?date=today&station=9419750&product=water_level&datum=MTL&time_zone=gmt&units=metric&format=csv')
+if (response.status_code == 200):
+	f.write(response.text)
+f.close()
