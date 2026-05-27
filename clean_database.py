d79da285726296cb706318025345ff5cd42435ab
Dobrea Matei-Stefan <matei.dobrea22@gmail.com>
2026-05-27 23:59:30 +0300

    Date transformed to time from epoch

--- clean_database.py
+++ clean_database.py
@@ -1,10 +1,13 @@
 import pandas as pd
+from datetime import datetime
 
 df = pd.read_csv("database_raw.csv")
 
 df.fillna({"fl_minor": df["fl_minor"].mean()}, inplace=True)
 df.fillna({"fl_moderate": df["fl_moderate"].mean()}, inplace=True)
 df.fillna({"fl_major": df["fl_major"].mean()}, inplace=True)
+
+df["Date Time"] = datetime.strptime(df["Date Time"][0], "%Y-%m-%d %H:%M").timestamp()
 
 df = df[df["name"] == "Crescent City"]
 
