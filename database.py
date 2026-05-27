3f1a3043bf393a7a8085998181fc6095230366fd
Dobrea Matei-Stefan <matei.dobrea22@gmail.com>
2026-05-27 17:29:37 +0300

    Database done with little issue with great lakes

--- database.py
+++ database.py
@@ -3,9 +3,10 @@ import database_maker.data_parser as data_parser
 from database_maker.proj_classes import attributes_t
 import pandas as pd
 import json
+import os
 
 # Station IDs of stations from around the USA
-ids = [9419750, 8570283, 8447435, 8452660, 8454000, 8461490, 8467150, 8518750, 8510560, 8534720, 8551910, 8548989, 8570283, 8632200, 8571892, 8574680, 8635750, 8594900, 8635750, 8638610, 8652587, 8658163, 8670870, 8724580, 8729210, 8766072, 8775296, 8779770, 9410230, 9412110, 9414523, 9416841, 9418767, 9432780, 9439040, 9443090, 9449424]
+ids = [9419750, 8570283, 8447435, 8452660, 8454000, 8461490, 8467150, 8518750, 8510560, 8534720, 8551910, 8548989, 8570283, 8632200, 8571892, 8574680, 8635750, 8594900, 8635750, 8638610, 8652587, 8658163, 8670870, 8724580, 8729210, 8766072, 8775296, 8779770, 9410230, 9412110, 9414523, 9416841, 9418767, 9432780, 9439040, 9443090, 9449424, 9450460, 9452634, 9454050, 9457804, 9459881, 9468333, 9464212, 9461710, 9099064, 9099018, 9087031, 9087057, 9087088, 9087044, 9075065, 9075014, 9063038, 9052076]
 # Smaller sample for testing purposes
 # +-----------------------------------------+
 # |		DO NOT FORGET TO CHANGE THE INPUT	|
@@ -16,7 +17,7 @@ ids_test = [9419750, 8570283, 8447435, 8452660, 8454000, 8461490, 8467150, 85187
 # Making a list with all the dataframes to concatenate them at the end
 df_list = []
 
-for id in ids_test:
+for id in ids:
 	valid = api_req_sender.get_info(id)
 
 	# Checking if the server responded or if the station has MLW datum
@@ -47,5 +48,11 @@ for id in ids_test:
 	else:
 		print(f"[WARNING] Ignoring station {id}", end="\n\n")
 
-	final_df = pd.concat(df_list)
-	pd.DataFrame.to_csv(final_df, "database.csv")
+final_df = pd.concat(df_list)
+pd.DataFrame.to_csv(final_df, "database.csv")
+
+if os.path.exists("./response_data.csv"):
+	os.remove("./response_data.csv")
+
+if os.path.exists("./response_metadata.json"):
+	os.remove("./response_metadata.json")
--- database_maker/data_parser.py
+++ database_maker/data_parser.py
@@ -5,7 +5,16 @@ def parse_data(attributes : attributes_t) -> pd.DataFrame:
 	df = pd.read_csv("response_data.csv")
 	df["name"] = attributes.name
 	df["great_lake"] = attributes.great_lake
-	df["flood_level"] = attributes.flood_level
+
+	df["fl_minor"] = attributes.flood_level["minor"]
+	df["fl_moderate"] = attributes.flood_level["moderate"]
+	df["fl_major"] = attributes.flood_level["major"]
+
 	df["lat"] = attributes.lat
 	df["long"] = attributes.long
+	df.drop(' O or I (for verified)', axis=1, inplace=True)
+	df.drop(' F', axis=1, inplace=True)
+	df.drop(' R', axis=1, inplace=True)
+	df.drop(' L', axis=1, inplace=True)
+	df.drop(' Quality ', axis=1, inplace=True)
 	return df
