e746ce9b1b4c986d8f5f9af2604b78db0a9e56ab
Dobrea Matei-Stefan <matei.dobrea22@gmail.com>
2026-05-27 14:34:18 +0300

    Response written to file

--- api_req_sender.py
+++ api_req_sender.py
@@ -1,24 +1,10 @@
 import requests
 
-#url = 'https://api.tidesandcurrents.noaa.gov/api'
-# data = {'station=': '9419750',
-# 	 	'date=': 'latest',
-# 		'product=' : 'water_level',
-# 		'datum=' : 'MTL',
-# 		'units=' : 'metric',
-# 		'time_zone=' : 'gmt',
-# 		}
-#response = requests.post(url, json=data)
+f = open("response.txt", "w")
 
 response = requests.get('https://api.tidesandcurrents.noaa.gov/api/prod/datagetter?date=today&station=9419750&product=water_level&datum=MTL&time_zone=gmt&units=metric&format=xml')
 
-print(response.text)
+if (response.status_code == 200):
+	f.write(response.text)
 
-# Check if the request was successful
-# if response.status_code == 200:
-# 		# Parse the JSON response into a dictionary
-# 	data = response.json()
-# 	print(f"Successfully fetched data for user: {data['login']}")
-# 	print(f"Public repos: {data['public_repos']}")
-# else:
-# 	print(f"Error: Received status code {response.status_code}")
+print(response.text)
