2590ea9d9ca0afeefb82436c871f433fbc09714d
Dobrea Matei-Stefan <matei.dobrea22@gmail.com>
2026-05-27 23:49:12 +0300

    Cleaned up flooding

--- Makefile
+++ Makefile
@@ -1,4 +1,7 @@
 .PHONY: api_req
 
+database: | database_raw.csv
+	python3 clean_database.py
+
 database_raw:
 	python3 database_raw.py
--- clean_database.py
+++ clean_database.py
@@ -1,2 +1,11 @@
 import pandas as pd
 
+df = pd.read_csv("database_raw.csv")
+
+df.fillna({"fl_minor": df["fl_minor"].mean()}, inplace=True)
+df.fillna({"fl_moderate": df["fl_moderate"].mean()}, inplace=True)
+df.fillna({"fl_major": df["fl_major"].mean()}, inplace=True)
+
+df = df[df["name"] == "Crescent City"]
+
+pd.DataFrame.to_csv(df, "database.csv")
