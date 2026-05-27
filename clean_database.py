f68827612aeaee36f5347c4f9e65c2ed0ac5218c
Savulescu Alexandra <alexandrasavulescu2006@gmail.com>
2026-05-28 00:40:28 +0300

    Normalization

--- clean_database.py
+++ clean_database.py
@@ -8,8 +8,28 @@ df.fillna({"fl_minor": df["fl_minor"].mean()}, inplace=True)
 df.fillna({"fl_moderate": df["fl_moderate"].mean()}, inplace=True)
 df.fillna({"fl_major": df["fl_major"].mean()}, inplace=True)
 
+# Converts date in seconds passed from Epoch
 df["Date Time"] = datetime.strptime(df["Date Time"][0], "%Y-%m-%d %H:%M").timestamp()
 
+# Drop redundant column
+df.drop(df.columns[0], axis=1, inplace=True)
+
 df = df[df["name"] == "Crescent City"]
+
+water_lvl_max = df[' Water Level'].max()
+
+# Normalizing Water Level
+scaler = MinMaxScaler()
+temp = pd.DataFrame(df[' Water Level'])
+df[' Water Level'] = scaler.fit_transform(temp)
+
+# Normalizing fl_minor
+df['fl_minor'] = df['fl_minor'] / water_lvl_max
+
+# Normalizing fl_moderate
+df['fl_moderate'] = df['fl_moderate'] / water_lvl_max
+
+# Normalizing fl_major
+df['fl_major'] = df['fl_major'] / water_lvl_max
 
 pd.DataFrame.to_csv(df, "database.csv")
